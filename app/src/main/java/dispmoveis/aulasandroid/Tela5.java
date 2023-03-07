6b440f5738084d08b1975c5fefe4345e4c25d69f
Del Caro <delcarof8@gmail.com>
2023-03-07 11:46:19 -0300

    Exercicios pronto de transiçoes de tela, 6 telas

--- app/src/main/java/dispmoveis/aulasandroid/Tela5.java
+++ app/src/main/java/dispmoveis/aulasandroid/Tela5.java
@@ -5,23 +5,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-public class Activity_Tela2 extends AppCompatActivity {
+public class Tela5 extends AppCompatActivity {
     Button pxpage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela2);
+        setContentView(R.layout.activity_tela5);
 
-        pxpage=findViewById(R.id.bp2);
+        pxpage=findViewById(R.id.bp4);
         pxpage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent in = new Intent(Activity_Tela2.this, Tela3.class);
+                Intent in = new Intent(Tela5.this, Tela6.class);
                 startActivity(in);
             }
         });
